7a405d0b2746e76a19767d92807199669763e5ff
Balla Kavya <ballakavya594@gmail.com>
2024-06-18 15:52:32 +0530

    "Refactored App.js to use react-router-dom, added Home component, and created separate Resume2 component with its own CSS file."

--- src/components/Handler.js
+++ src/components/Handler.js
@@ -1,113 +1,164 @@
-export const handleChange = (e, formData, setFormData) => {
-    const { name, value } = e.target;
+// Handler.js
 
-    if (name === 'profile') {
-        let profileText = '';
-        switch (value) {
-            case 'fullStack':
-                profileText = "I am an experienced full-stack developer with expertise in frontend (HTML/CSS/JavaScript, React) and backend (Node.js, Express) technologies. Skilled in building scalable applications, optimizing performance, and implementing best practices. Passionate about problem-solving and creating seamless user experiences.";
-                break;
-            case 'software':
-                profileText = "I am a software developer adept in designing and implementing robust applications. Proficient in multiple programming languages and frameworks, with a strong focus on code quality, scalability, and innovation. Committed to delivering solutions that meet both technical and business requirements effectively.";
-                break;
-            case 'mernStack':
-                profileText = "I am a MERN stack developer proficient in MongoDB, Express.js, React, and Node.js. Experienced in building full-stack applications, integrating APIs, and optimizing performance. Skilled in frontend and backend development, with a focus on creating responsive, user-friendly interfaces and scalable solutions.";
-                break;
-            case 'appDeveloper':
-                profileText = "I am an app developer skilled in designing and deploying native and hybrid applications for iOS and Android platforms. Proficient in UI/UX design, mobile development frameworks, and implementing robust backend solutions. Dedicated to creating intuitive user experiences and optimizing app performance for diverse user bases.";
-                break;
-            case 'frontEnd':
-                profileText = "I am a dedicated front-end web developer proficient in HTML, CSS, and JavaScript frameworks such as React and Vue.js. Experienced in creating responsive, user-friendly interfaces and optimizing web performance. Passionate about crafting visually appealing websites that enhance user experience and achieve business objectives.";
-                break;
-            default:
-                profileText = '';
-        }
+import { useState, useEffect } from 'react';
 
-        setFormData({ ...formData, profile: value, profileText: profileText });
-    } else {
-        const keys = name.split('.');
-        if (keys.length > 1) {
-            setFormData(prevState => ({
-                ...prevState,
-                [keys[0]]: {
-                    ...prevState[keys[0]],
-                    [keys[1]]: value
-                }
-            }));
+const useFormHandlers = () => {
+    const [loading, setLoading] = useState(true);
+    const [formData, setFormData] = useState({
+        name: '',
+        title: '',
+        contact: {
+            phone: '',
+            email: '',
+            linkedin: '',
+            github: '',
+            address: '',
+        },
+        profile: '',
+        profileText: '',
+        skills: [],
+        education: [],
+        experience: [],
+        certificates: [],
+        languages: [],
+    });
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 3000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        if (name === 'profile') {
+            let profileText = '';
+            switch (value) {
+                case 'fullStack':
+                    profileText = "I am an experienced full-stack developer with expertise in frontend (HTML/CSS/JavaScript, React) and backend (Node.js, Express) technologies. Skilled in building scalable applications, optimizing performance, and implementing best practices. Passionate about problem-solving and creating seamless user experiences.";
+                    break;
+                case 'software':
+                    profileText = "I am a software developer adept in designing and implementing robust applications. Proficient in multiple programming languages and frameworks, with a strong focus on code quality, scalability, and innovation. Committed to delivering solutions that meet both technical and business requirements effectively.";
+                    break;
+                case 'mernStack':
+                    profileText = "I am a MERN stack developer proficient in MongoDB, Express.js, React, and Node.js. Experienced in building full-stack applications, integrating APIs, and optimizing performance. Skilled in frontend and backend development, with a focus on creating responsive, user-friendly interfaces and scalable solutions.";
+                    break;
+                case 'appDeveloper':
+                    profileText = "I am an app developer skilled in designing and deploying native and hybrid applications for iOS and Android platforms. Proficient in UI/UX design, mobile development frameworks, and implementing robust backend solutions. Dedicated to creating intuitive user experiences and optimizing app performance for diverse user bases.";
+                    break;
+                case 'frontEnd':
+                    profileText = "I am a dedicated front-end web developer proficient in HTML, CSS, and JavaScript frameworks such as React and Vue.js. Experienced in creating responsive, user-friendly interfaces and optimizing web performance. Passionate about crafting visually appealing websites that enhance user experience and achieve business objectives.";
+                    break;
+                default:
+                    profileText = '';
+            }
+
+            setFormData({ ...formData, profile: value, profileText: profileText });
         } else {
-            setFormData({ ...formData, [name]: value });
+            const keys = name.split('.');
+            if (keys.length > 1) {
+                setFormData(prevState => ({
+                    ...prevState,
+                    [keys[0]]: {
+                        ...prevState[keys[0]],
+                        [keys[1]]: value
+                    }
+                }));
+            } else {
+                setFormData({ ...formData, [name]: value });
+            }
         }
-    }
-};
-
-export const handleArrayChange = (e, index, field, formData, setFormData) => {
-    const newValue = e.target.value;
-    const newArray = [...formData[field]];
-    newArray[index] = newValue;
-    setFormData({
-        ...formData,
-        [field]: newArray,
-    });
-};
-
-export const handleNestedArrayChange = (e, index, field, parentField, formData, setFormData) => {
-    const newValue = e.target.value;
-    const newArray = [...formData[parentField]];
-    newArray[index] = {
-        ...newArray[index],
-        [field]: newValue,
     };
-    setFormData({
-        ...formData,
-        [parentField]: newArray,
-    });
+
+    const handleArrayChange = (e, index, field) => {
+        const newValue = e.target.value;
+        const newArray = [...formData[field]];
+        newArray[index] = newValue;
+        setFormData({
+            ...formData,
+            [field]: newArray,
+        });
+    };
+
+    const handleNestedArrayChange = (e, index, field, parentField) => {
+        const newValue = e.target.value;
+        const newArray = [...formData[parentField]];
+        newArray[index] = {
+            ...newArray[index],
+            [field]: newValue,
+        };
+        setFormData({
+            ...formData,
+            [parentField]: newArray,
+        });
+    };
+
+    const addSkill = () => {
+        setFormData({
+            ...formData,
+            skills: [...formData.skills, ''],
+        });
+    };
+
+    const addEducation = () => {
+        setFormData({
+            ...formData,
+            education: [...formData.education, { degree: '', institution: '', startYear: '', endYear: '' }],
+        });
+    };
+
+    const addCertificate = () => {
+        setFormData({
+            ...formData,
+            certificates: [...formData.certificates, ''],
+        });
+    };
+
+    const addLanguage = () => {
+        setFormData({
+            ...formData,
+            languages: [...formData.languages, ''],
+        });
+    };
+
+    const handleAddExperience = () => {
+        setFormData({
+            ...formData,
+            experience: [...formData.experience, { position: '', company: '', startMonth: '', startYear: '', endMonth: '', endYear: '', internships: '' }],
+        });
+    };
+
+    const handleDelete = (index, field) => {
+        const newArray = [...formData[field]];
+        newArray.splice(index, 1);
+        setFormData({
+            ...formData,
+            [field]: newArray,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Form submitted', formData);
+    };
+
+    return {
+        loading,
+        formData,
+        handleChange,
+        handleArrayChange,
+        handleNestedArrayChange,
+        addSkill,
+        addEducation,
+        addCertificate,
+        addLanguage,
+        handleAddExperience,
+        handleDelete,
+        handleSubmit
+    };
 };
 
-export const addSkill = (formData, setFormData) => {
-    setFormData({
-        ...formData,
-        skills: [...formData.skills, ''],
-    });
-};
-
-export const addEducation = (formData, setFormData) => {
-    setFormData({
-        ...formData,
-        education: [...formData.education, { degree: '', institution: '', startYear: '', endYear: '' }],
-    });
-};
-
-export const addCertificate = (formData, setFormData) => {
-    setFormData({
-        ...formData,
-        certificates: [...formData.certificates, ''],
-    });
-};
-
-export const addLanguage = (formData, setFormData) => {
-    setFormData({
-        ...formData,
-        languages: [...formData.languages, ''],
-    });
-};
-
-export const handleAddExperience = (formData, setFormData) => {
-    setFormData({
-        ...formData,
-        experience: [...formData.experience, { position: '', company: '', startMonth: '', startYear: '', endMonth: '', endYear: '', internships: '' }],
-    });
-};
-
-export const handleDelete = (index, field, formData, setFormData) => {
-    const newArray = [...formData[field]];
-    newArray.splice(index, 1);
-    setFormData({
-        ...formData,
-        [field]: newArray,
-    });
-};
-
-export const handleSubmit = (e, formData) => {
-    e.preventDefault();
-    console.log('Form submitted', formData);
-};
+export default useFormHandlers;
