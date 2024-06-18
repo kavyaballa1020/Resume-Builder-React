63a1f95881e7a3517bdf2762c35e12818bf12230
Balla Kavya <ballakavya594@gmail.com>
2024-06-18 12:24:31 +0530

    Update

---
+++ src/components/Preloader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Preloader.css';
+
+const Preloader = () => {
+    return (
+        <div className="preloader">
+            <div className="spinner"></div>
+        </div>
+    );
+};
+
+export default Preloader;
