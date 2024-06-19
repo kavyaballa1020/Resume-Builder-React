cc76e2908e1bab09808c24ff293a915f1d08492a
Balla Kavya <ballakavya594@gmail.com>
2024-06-19 15:28:55 +0530

    "Deleted file: src/components/Resume 2.jpg"

--- src/components/Preloader.js
+++ src/components/Preloader.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Preloader.css';
-import KavyaImage from './Kavya.JPG';  // Adjust the path as necessary
+import KavyaImage from './K.jpg';  // Adjust the path as necessary
 
 const Preloader = () => {
     return (
         <div className="preloader">
-            <h1 className="brand-name"><i className="fas fa-fire"></i> Kavya-Resume-Builder</h1>
+            <h1 className="brand-name1"><i className="fas fa-fire"></i> Kavya-Resume-Builder</h1>
             <div className="spinner"></div>
             <div className="welcome-text">
                 <h1>👋 Welcome to My Resume Builder webpage! 🚀</h1>
